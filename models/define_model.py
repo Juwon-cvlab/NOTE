4d8fba8d400ede7397c67e06c5f207b149abc85e
Juwon_Kang <gjw0917@postech.ac.kr>
2023-09-04 15:46:43 +0900

    default model_dir=ckpt

--- models/define_model.py
+++ models/define_model.py
@@ -3,7 +3,7 @@ from robustbench.model_zoo.enums import ThreatModel
 
 def define_model(args):
     if args.model == 'wideresnet28-10':
-        model = load_model('Strandard', dataset='cifar10', threat_model=ThreatModel.corruptions)
+        model = load_model('Strandard', model_dir='./ckpt', dataset='cifar10', threat_model=ThreatModel.corruptions)
     elif args.model == 'wideresnet40-2':
-        model = load_model('Hendrycks2020AugMix_WRN', dataset='cifar10', threat_model=ThreatModel.corruptions)
+        model = load_model('Hendrycks2020AugMix_WRN', model_dir='./ckpt', dataset='cifar10', threat_model=ThreatModel.corruptions)
     return model
